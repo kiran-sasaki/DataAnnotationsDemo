145bea9a2355f1208dd4c14281a1fb0c1294859f
kiran-sasaki <kiransekar1111@gmail.com>
2023-01-03 16:46:19 +0530

    [Kiran] Add: Data Annotations

---
+++ DataAnnotations/AnnotationTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotations
+{
+    public static class AnnotationTest
+    {
+        public static void Display()
+        {
+            Console.WriteLine("Employee validation");
+            Console.WriteLine("--------------------------");
+            Employee employee = new Employee();
+            employee.Name = "Kiran";
+            employee.Age = 10;
+            employee.PhoneNumber = "5678334565";
+
+            ValidationContext context = new ValidationContext(employee, null, null);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(employee,context,validationResults,true);
+            if(!valid)
+            {
+                foreach(ValidationResult validationResult in validationResults)
+                {
+                    Console.Write("Member Name: {0}",validationResult.MemberNames.First(),Environment.NewLine);
+                    Console.Write("Error message: {0}{1}",validationResult.ErrorMessage,Environment.NewLine);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Name: "+employee.Name+"Age: "+employee.Age+"PhnNo: "+employee.PhoneNumber);
+
+            }
+            Console.WriteLine("Exit");
+
+        }
+    }
+}
--- DataAnnotations/Program.cs
+++ DataAnnotations/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Data Annotation Problem");
+            AnnotationTest.Display();
         }
     }
 }
